6591a9b4c30d430fad987e62dbe6965d28900fd2
Tim Coull <timcou93@gmail.com>
2020-04-23 09:44:52 +0200

    Updated core loop and audio.
    Added class for game loop and added OpenAL parallell thread running.

---
+++ Vf/Vf/audio.cpp
@@ -0,0 +1,427 @@
+#include "Audio.h"
+
+Audio::Audio() {}
+Audio::~Audio() {}
+
+bool Audio::check_al_errors(const std::string& filename, const std::uint_fast32_t line)
+{
+	ALenum error = alGetError();
+	if (error != AL_NO_ERROR)
+	{
+		std::cerr << "***ERROR*** (" << filename << ": " << line << ")\n";
+		switch (error)
+		{
+		case AL_INVALID_NAME:
+			std::cerr << "AL_INVALID_NAME: a bad name (ID) was passed to an OpenAL function";
+			break;
+		case AL_INVALID_ENUM:
+			std::cerr << "AL_INVALID_ENUM: an invalid enum value was passed to an OpenAL function";
+			break;
+		case AL_INVALID_VALUE:
+			std::cerr << "AL_INVALID_VALUE: an invalid value was passed to an OpenAL function";
+			break;
+		case AL_INVALID_OPERATION:
+			std::cerr << "AL_INVALID_OPERATION: the requested operation is not valid";
+			break;
+		case AL_OUT_OF_MEMORY:
+			std::cerr << "AL_OUT_OF_MEMORY: the requested operation resulted in OpenAL running out of memory";
+			break;
+		default:
+			std::cerr << "UNKNOWN AL ERROR: " << error;
+		}
+		std::cerr << std::endl;
+		return false;
+	}
+	return true;
+}
+
+bool Audio::check_alc_errors(const std::string& filename, const std::uint_fast32_t line, ALCdevice* device)
+{
+	ALCenum error = alcGetError(device);
+	if (error != ALC_NO_ERROR)
+	{
+		std::cerr << "***ERROR*** (" << filename << ": " << line << ")\n";
+		switch (error)
+		{
+		case ALC_INVALID_VALUE:
+			std::cerr << "ALC_INVALID_VALUE: an invalid value was passed to an OpenAL function";
+			break;
+		case ALC_INVALID_DEVICE:
+			std::cerr << "ALC_INVALID_DEVICE: a bad device was passed to an OpenAL function";
+			break;
+		case ALC_INVALID_CONTEXT:
+			std::cerr << "ALC_INVALID_CONTEXT: a bad context was passed to an OpenAL function";
+			break;
+		case ALC_INVALID_ENUM:
+			std::cerr << "ALC_INVALID_ENUM: an unknown enum value was passed to an OpenAL function";
+			break;
+		case ALC_OUT_OF_MEMORY:
+			std::cerr << "ALC_OUT_OF_MEMORY: an unknown enum value was passed to an OpenAL function";
+			break;
+		default:
+			std::cerr << "UNKNOWN ALC ERROR: " << error;
+		}
+		std::cerr << std::endl;
+		return false;
+	}
+	return true;
+}
+
+template<typename alFunction, typename... Params>
+auto Audio::alCallImpl(const char* filename,
+	const std::uint_fast32_t line,
+	alFunction function,
+	Params... params)
+	->typename std::enable_if_t<!std::is_same_v<void, decltype(function(params...))>, decltype(function(params...))>
+{
+	auto ret = function(std::forward<Params>(params)...);
+	check_al_errors(filename, line);
+	return ret;
+}
+
+template<typename alFunction, typename... Params>
+auto Audio::alCallImpl(const char* filename,
+	const std::uint_fast32_t line,
+	alFunction function,
+	Params... params)
+	->typename std::enable_if_t<std::is_same_v<void, decltype(function(params...))>, bool>
+{
+	function(std::forward<Params>(params)...);
+	return check_al_errors(filename, line);
+}
+
+template<typename alcFunction, typename... Params>
+auto Audio::alcCallImpl(const char* filename,
+	const std::uint_fast32_t line,
+	alcFunction function,
+	ALCdevice* device,
+	Params... params)
+	->typename std::enable_if_t<std::is_same_v<void, decltype(function(params...))>, bool>
+{
+	function(std::forward<Params>(params)...);
+	return check_alc_errors(filename, line, device);
+}
+
+template<typename alcFunction, typename ReturnType, typename... Params>
+auto Audio::alcCallImpl(const char* filename,
+	const std::uint_fast32_t line,
+	alcFunction function,
+	ReturnType& returnValue,
+	ALCdevice* device,
+	Params... params)
+	->typename std::enable_if_t<!std::is_same_v<void, decltype(function(params...))>, bool>
+{
+	returnValue = function(std::forward<Params>(params)...);
+	return check_alc_errors(filename, line, device);
+}
+
+std::int32_t Audio::convert_to_int(char* buffer, std::size_t len)
+{
+	std::int32_t a = 0;
+	if (endian::native == endian::little)
+		std::memcpy(&a, buffer, len);
+	else
+		for (std::size_t i = 0; i < len; ++i)
+			reinterpret_cast<char*>(&a)[3 - i] = buffer[i];
+	return a;
+}
+
+bool Audio::load_wav_file_header(std::ifstream& file,
+	std::uint8_t& channels,
+	std::int32_t& sampleRate,
+	std::uint8_t& bitsPerSample,
+	ALsizei& size)
+{
+	char buffer[4];
+	if (!file.is_open())
+		return false;
+
+	// the RIFF
+	if (!file.read(buffer, 4))
+	{
+		std::cerr << "ERROR: could not read RIFF" << std::endl;
+		return false;
+	}
+	if (std::strncmp(buffer, "RIFF", 4) != 0)
+	{
+		std::cerr << "ERROR: file is not a valid WAVE file (header doesn't begin with RIFF)" << std::endl;
+		return false;
+	}
+
+	// the size of the file
+	if (!file.read(buffer, 4))
+	{
+		std::cerr << "ERROR: could not read size of file" << std::endl;
+		return false;
+	}
+
+	// the WAVE
+	if (!file.read(buffer, 4))
+	{
+		std::cerr << "ERROR: could not read WAVE" << std::endl;
+		return false;
+	}
+	if (std::strncmp(buffer, "WAVE", 4) != 0)
+	{
+		std::cerr << "ERROR: file is not a valid WAVE file (header doesn't contain WAVE)" << std::endl;
+		return false;
+	}
+
+	// "fmt/0"
+	if (!file.read(buffer, 4))
+	{
+		std::cerr << "ERROR: could not read fmt/0" << std::endl;
+		return false;
+	}
+
+	// this is always 16, the size of the fmt data chunk
+	if (!file.read(buffer, 4))
+	{
+		std::cerr << "ERROR: could not read the 16" << std::endl;
+		return false;
+	}
+
+	// PCM should be 1?
+	if (!file.read(buffer, 2))
+	{
+		std::cerr << "ERROR: could not read PCM" << std::endl;
+		return false;
+	}
+
+	// the number of channels
+	if (!file.read(buffer, 2))
+	{
+		std::cerr << "ERROR: could not read number of channels" << std::endl;
+		return false;
+	}
+	channels = convert_to_int(buffer, 2);
+
+	// sample rate
+	if (!file.read(buffer, 4))
+	{
+		std::cerr << "ERROR: could not read sample rate" << std::endl;
+		return false;
+	}
+	sampleRate = convert_to_int(buffer, 4);
+
+	// (sampleRate * bitsPerSample * channels) / 8
+	if (!file.read(buffer, 4))
+	{
+		std::cerr << "ERROR: could not read (sampleRate * bitsPerSample * channels) / 8" << std::endl;
+		return false;
+	}
+
+	// ?? dafaq
+	if (!file.read(buffer, 2))
+	{
+		std::cerr << "ERROR: could not read dafaq" << std::endl;
+		return false;
+	}
+
+	// bitsPerSample
+	if (!file.read(buffer, 2))
+	{
+		std::cerr << "ERROR: could not read bits per sample" << std::endl;
+		return false;
+	}
+	bitsPerSample = convert_to_int(buffer, 2);
+
+	// data chunk header "data"
+	if (!file.read(buffer, 4))
+	{
+		std::cerr << "ERROR: could not read data chunk header" << std::endl;
+		return false;
+	}
+	if (std::strncmp(buffer, "data", 4) != 0)
+	{
+		std::cerr << "ERROR: file is not a valid WAVE file (doesn't have 'data' tag)" << std::endl;
+		return false;
+	}
+
+	// size of data
+	if (!file.read(buffer, 4))
+	{
+		std::cerr << "ERROR: could not read data size" << std::endl;
+		return false;
+	}
+	size = convert_to_int(buffer, 4);
+
+	/* cannot be at the end of file */
+	if (file.eof())
+	{
+		std::cerr << "ERROR: reached EOF on the file" << std::endl;
+		return false;
+	}
+	if (file.fail())
+	{
+		std::cerr << "ERROR: fail state set on the file" << std::endl;
+		return false;
+	}
+
+	return true;
+}
+
+char* Audio::load_wav(const std::string& filename,
+	std::uint8_t& channels,
+	std::int32_t& sampleRate,
+	std::uint8_t& bitsPerSample,
+	ALsizei& size)
+{
+	std::ifstream in(filename, std::ios::binary);
+	if (!in.is_open())
+	{
+		std::cerr << "ERROR: Could not open \"" << filename << "\"" << std::endl;
+		return nullptr;
+	}
+	if (!load_wav_file_header(in, channels, sampleRate, bitsPerSample, size))
+	{
+		std::cerr << "ERROR: Could not load wav header of \"" << filename << "\"" << std::endl;
+		return nullptr;
+	}
+
+	char* data = new char[size];
+
+	in.read(data, size);
+
+	return data;
+}
+
+const std::size_t NUM_BUFFERS = 4;
+const std::size_t BUFFER_SIZE = 65536; // 32kb of data in each buffer
+
+void Audio::update_stream(const ALuint source,
+	const ALenum& format,
+	const std::int32_t& sampleRate,
+	const std::vector<char>& soundData,
+	std::size_t& cursor)
+{
+	ALint buffersProcessed = 0;
+	alCall(alGetSourcei, source, AL_BUFFERS_PROCESSED, &buffersProcessed);
+
+	if (buffersProcessed <= 0)
+		return;
+
+	while (buffersProcessed--)
+	{
+		ALuint buffer;
+		alCall(alSourceUnqueueBuffers, source, 1, &buffer);
+
+		ALsizei dataSize = BUFFER_SIZE;
+
+		char* data = new char[dataSize];
+		std::memset(data, 0, dataSize);
+
+		std::size_t dataSizeToCopy = BUFFER_SIZE;
+		if (cursor + BUFFER_SIZE > soundData.size())
+			dataSizeToCopy = soundData.size() - cursor;
+
+		std::memcpy(&data[0], &soundData[cursor], dataSizeToCopy);
+		cursor += dataSizeToCopy;
+
+		if (dataSizeToCopy < BUFFER_SIZE)
+		{
+			cursor = 0;
+			std::memcpy(&data[dataSizeToCopy], &soundData[cursor], BUFFER_SIZE - dataSizeToCopy);
+			cursor = BUFFER_SIZE - dataSizeToCopy;
+		}
+
+		alCall(alBufferData, buffer, format, data, BUFFER_SIZE, sampleRate);
+		alCall(alSourceQueueBuffers, source, 1, &buffer);
+
+		delete[] data;
+	}
+}
+
+int Audio::playSound(std::string soundToPlay)
+{
+	ALCdevice* openALDevice = alcOpenDevice(nullptr);
+	if (!openALDevice)
+		return 0;
+
+	ALCcontext* openALContext;
+	if (!alcCall(alcCreateContext, openALContext, openALDevice, openALDevice, nullptr) || !openALContext)
+	{
+		std::cerr << "ERROR: Could not create audio context" << std::endl;
+		return 0;
+	}
+	ALCboolean contextMadeCurrent = false;
+	if (!alcCall(alcMakeContextCurrent, contextMadeCurrent, openALDevice, openALContext)
+		|| contextMadeCurrent != ALC_TRUE)
+	{
+		std::cerr << "ERROR: Could not make audio context current" << std::endl;
+		return 0;
+	}
+
+	std::uint8_t channels;
+	std::int32_t sampleRate;
+	std::uint8_t bitsPerSample;
+	ALsizei dataSize;
+	char* rawSoundData = load_wav(soundToPlay, channels, sampleRate, bitsPerSample, dataSize);
+	if (rawSoundData == nullptr || dataSize == 0)
+	{
+		std::cerr << "ERROR: Could not load wav" << std::endl;
+		return 0;
+	}
+	std::vector<char> soundData(rawSoundData, rawSoundData + dataSize);
+
+	ALuint buffers[NUM_BUFFERS];
+
+	alCall(alGenBuffers, NUM_BUFFERS, &buffers[0]);
+
+	ALenum format;
+
+	if (channels == 1 && bitsPerSample == 8)
+		format = AL_FORMAT_MONO8;
+	else if (channels == 1 && bitsPerSample == 16)
+		format = AL_FORMAT_MONO16;
+	else if (channels == 2 && bitsPerSample == 8)
+		format = AL_FORMAT_STEREO8;
+	else if (channels == 2 && bitsPerSample == 16)
+		format = AL_FORMAT_STEREO16;
+	else
+	{
+		std::cerr
+			<< "ERROR: unrecognised wave format: "
+			<< channels << " channels, "
+			<< bitsPerSample << " bps" << std::endl;
+		return 0;
+	}
+
+	for (std::size_t i = 0; i < NUM_BUFFERS; ++i)
+	{
+		alCall(alBufferData, buffers[i], format, &soundData[i * BUFFER_SIZE], BUFFER_SIZE, sampleRate);
+	}
+
+	ALuint source;
+	alCall(alGenSources, 1, &source);
+	alCall(alSourcef, source, AL_PITCH, 1);
+	alCall(alSourcef, source, AL_GAIN, 1.0f);
+	alCall(alSource3f, source, AL_POSITION, 0, 0, 0);
+	alCall(alSource3f, source, AL_VELOCITY, 0, 0, 0);
+	alCall(alSourcei, source, AL_LOOPING, AL_FALSE);
+
+	alCall(alSourceQueueBuffers, source, NUM_BUFFERS, &buffers[0]);
+
+	alCall(alSourcePlay, source);
+
+	ALint state = AL_PLAYING;
+
+	std::size_t cursor = BUFFER_SIZE * NUM_BUFFERS;
+
+	while (state == AL_PLAYING)
+	{
+		update_stream(source, format, sampleRate, soundData, cursor);
+		alCall(alGetSourcei, source, AL_SOURCE_STATE, &state);
+	}
+
+	alCall(alDeleteSources, 1, &source);
+	alCall(alDeleteBuffers, NUM_BUFFERS, &buffers[0]);
+
+	alcCall(alcMakeContextCurrent, contextMadeCurrent, openALDevice, nullptr);
+	alcCall(alcDestroyContext, openALDevice, openALContext);
+
+	ALCboolean closed;
+	//alcCall(alcCloseDevice, closed, openALDevice, openALDevice);
+
+	return 0;
+}
--- Vf/Vf/main.cpp
+++ Vf/Vf/main.cpp
@@ -1,76 +1,34 @@
-#define _CRT_SECURE_NO_DEPRECATE
-#include <stdio.h>
-#include <iostream> 
-#include <fstream>
-// BGFX
-#include <bgfx/bgfx.h>
-#include <bgfx/platform.h>
+#include "Game.h"
 
-// BX
-#include <bx/math.h>
+Game* game = nullptr;
+Audio* audio = nullptr;
 
-// GLEW 
-#define GLEW_STATIC
-#include <GL/glew.h> 
+void gameThread() {
+	game = new Game();
+	game->init("Voidfall Alpha", 0, 0, 1200, 720, false);
+	while (game->running()) {
 
-#include "audio.h"
+		game->handleEvents();
+		game->update();
+		game->render();
+	}
+	
+	game->clean();
+	return;
+}
 
-// GLFW 
-#include <GLFW/glfw3.h> 
-#define GLFW_EXPOSE_NATIVE_WIN32
-#include "GLFW/glfw3native.h"
-
-
-
-
-// Window dimensions 
-#define WNDW_WIDTH 1600
-#define WNDW_HEIGHT 900
-
-
-
+void audioThread() {
+	audio = new Audio();
+	audio->playSound("../testy.wav");
+}
 
 int main() {
 
-	glfwInit();
-	GLFWwindow* window = glfwCreateWindow(WNDW_WIDTH, WNDW_HEIGHT, "Hello, bgfx!", NULL, NULL);
-	
+	std::thread t1(gameThread);
+	std::thread t2(audioThread);
 
-	bgfx::PlatformData pd;
-	pd.nwh = glfwGetWin32Window(window);
-	bgfx::setPlatformData(pd);
-	bgfx::renderFrame();
+	t1.join();
+	t2.join();
 
-	bgfx::init();
-
-	bgfx::reset(WNDW_WIDTH, WNDW_HEIGHT, BGFX_RESET_VSYNC);
-
-	bgfx::setDebug(BGFX_DEBUG_TEXT /*| BGFX_DEBUG_STATS*/);
-
-    // Enable debug text.
-    bgfx::setDebug(BGFX_DEBUG_TEXT /*| BGFX_DEBUG_STATS*/);
-
-    // Set view rectangle for 0th view
-    bgfx::setViewRect(0, 0, 0, uint16_t(WNDW_WIDTH), uint16_t(WNDW_HEIGHT));
-
-    // Clear the view rect
-    bgfx::setViewClear(0,
-        BGFX_CLEAR_COLOR | BGFX_CLEAR_DEPTH,
-        0x443355FF, 1.0f, 0);
-
-    // Set empty primitive on screen
-    bgfx::touch(0);
-    
-
-    // Currently the frame gets rendered but the sound buffer takes over and plays the track without updating the graphics
-    while(true){
-        bgfx::frame();
-        playSound("../testy.wav");
-    }
-      
-
-
-    bgfx::shutdown();
-    
 }
 
