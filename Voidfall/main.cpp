a4e46dbcfebe048f52cb56bdcf1302195fe632bf
CuriouslyCurious <thecuriouslycurious@protonmail.com>
2020-04-25 16:57:18 +0200

    Add the proper main.cpp from master

--- Voidfall/main.cpp
+++ Voidfall/main.cpp
@@ -1,76 +1,36 @@
-#include <stdio.h>
-#include <iostream>
-#include <fstream>
+#include "Game.h"
+#include "Audio.h"
+#include <thread>
 
-// BGFX
-//#include <bx/bx.h>
-#include <bgfx/bgfx.h>
-#include <bgfx/platform.h>
+Game* game = nullptr;
+Audio* audio = nullptr;
 
-// GLEW
-//#define GLEW_STATIC
-//#include <GL/glew.h>
+void gameThread() {
+	game = new Game();
+	game->init("Voidfall Alpha", 0, 0, 1200, 720, false);
+	while (game->running()) {
 
-// GLFW
-#if BX_PLATFORM_LINUX
-#define GLFW_EXPOSE_NATIVE_X11
-#elif BX_PLATFORM_WINDOWS
-#define GLFW_EXPOSE_NATIVE_WIN32
-#elif BX_PLATFORM_OSX
-#define GLFW_EXPOSE_NATIVE_COCOA
-#endif
-//#include "logo.h"
-#include <GLFW/glfw3.h>
-#include <GLFW/glfw3native.h>
+		game->handleEvents();
+		game->update();
+		game->render();
+	}
 
-// Window dimensions
-const GLint WIDTH = 800, HEIGHT = 600;
-uint32_t reset = BGFX_RESET_VSYNC;
-
-int main(void)
-{
-	glfwInit();
-
-	GLFWwindow *window = glfwCreateWindow(WIDTH, HEIGHT, "no, bgfx!", nullptr, nullptr);
-    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-	if (!window)
-		return 1;
-	//glfwSetKeyCallback(window, glfw_keyCallback);
-
-    bgfx::renderFrame();
-
-    bgfx::Init init;
-    #if BX_PLATFORM_LINUX || BX_PLATFORM_BSD
-	init.platformData.ndt = glfwGetX11Display();
-	init.platformData.nwh = (void*)(uintptr_t)glfwGetX11Window(window);
-    #elif BX_PLATFORM_OSX
-	init.platformData.nwh = glfwGetCocoaWindow(window);
-    #elif BX_PLATFORM_WINDOWS
-	init.platformData.nwh = glfwGetWin32Window(window);
-    #endif
-
-	//bgfx_set_platform_data;
-
-	//bgfx_init_t bgfxInit;
-
-	// Automactiaclly choose renderer.
-	//init.type = BGFX_RENDERER_TYPE_COUNT;
-
-	init.resolution.width = WIDTH;
-	init.resolution.height = HEIGHT;
-	init.resolution.reset = BGFX_RESET_VSYNC;
-
-	// Returns true if init is successful
-    if (!bgfx::init(init))
-        return 1;
-
-    const bgfx::ViewId kClearView = 0;
-	bgfx::setViewClear(kClearView, BGFX_CLEAR_COLOR);
-	bgfx::setViewRect(kClearView, 0, 0, bgfx::BackbufferRatio::Equal);
-
-	while(true)
-		bgfx::frame();
-
-    bgfx::shutdown();
-	return 0;
+	game->clean();
+	return;
 }
+
+void audioThread() {
+	audio = new Audio();
+	audio->playSound("../sounds/wav/ut.wav");
+}
+
+int main() {
+
+	std::thread t1(gameThread);
+	std::thread t2(audioThread);
+
+	t1.join();
+	t2.join();
+
+}
+
